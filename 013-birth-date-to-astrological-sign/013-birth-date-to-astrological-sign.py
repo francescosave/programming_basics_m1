1bbed3ed7e971c870f19fa266509a802b9ecdd13
Francesco Ricci <francescosave.ricci@gmail.com>
2021-12-29 08:49:07 +0100

    fix fixato l'input per splittare i due valori letti

--- 013-birth-date-to-astrological-sign/013-birth-date-to-astrological-sign.py
+++ 013-birth-date-to-astrological-sign/013-birth-date-to-astrological-sign.py
@@ -14,6 +14,15 @@ def getZodiacalName(month_name,day):
 
 month_name = 'gennaio'
 day = 1
+
+#input body
+user_input = input('Inserire il mese e il giorno di nascita')
+print(user_input)
+#fare lo split dei due valori
+user_input = user_input.split()
+print(user_input[0])
+print(user_input[1])
+
 message = "Sei nato il 1 gennaio e il tuo segno zodiacale è Capricorno"
 
 
