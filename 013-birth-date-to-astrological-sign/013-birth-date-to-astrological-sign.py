90a63454eea2716f734539b5d4d34cbc03546873
Francesco Ricci <francescosave.ricci@gmail.com>
2021-12-29 08:36:41 +0100

    feat Insert structure soruce code in 013-birt-date... py file

---
+++ 013-birth-date-to-astrological-sign/013-birth-date-to-astrological-sign.py
@@ -0,0 +1,22 @@
+# Inserire il nome del mese di nascita e il giorno di nascita.L'utente non deve preoccuparsi di inserire i due valori in un formato particolare.
+# # Esempio l'utente inserisce Dicembre 1 oppure gennaio 30.
+#
+# Creare un funzione che passando i due parametri letti restituisce il segno zodiacale.
+#
+# Mostrare un output tipo "Sei nato il 9 maggio e il tuo segno Zodiacale e Scorpione"
+
+
+
+def getZodiacalName(month_name,day):
+    #body function
+    return
+
+
+month_name = 'gennaio'
+day = 1
+message = "Sei nato il 1 gennaio e il tuo segno zodiacale è Capricorno"
+
+
+
+message = getZodiacalName(month_name,day)
+print(message)
