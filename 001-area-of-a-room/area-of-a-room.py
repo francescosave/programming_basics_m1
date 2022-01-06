7f3686f2fcbad36e8f28f35da8a6f2b86f23834b
Francesco Ricci <francescosave.ricci@gmail.com>
2022-01-06 22:31:21 +0100

    refacoring variables name and output message

---
+++ 001-area-of-a-room/area-of-a-room.py
@@ -0,0 +1,16 @@
+# esempio m1/area-of-a-room
+# refactoring nomi variabili e messaggi output
+
+room_width   =   10.50
+room_lenght    =   20.60
+
+room_width = float(input('digit room width :  '))
+room_lenght = float(input('digit room lenght :  '))
+
+print('\nThe width of the room is: ' + str(room_width))
+print('The lenght of the room is: ' + str(room_lenght))
+
+room_area = room_width + room_lenght
+room_area = room_width + room_lenght
+
+print("The room area is: " + str(room_area) + ' square meter')
