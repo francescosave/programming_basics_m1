32f54ac5ffb321f35c2c058ab647731407d4e430
Francesco Ricci <francescosave.ricci@gmail.com>
2022-01-07 22:45:16 +0100

    refacoring exercise 010 by variables name and output message

--- 010-month-name-to-number-of-days/010-month-name-to-number-of-days.py
+++ 010-month-name-to-number-of-days/010-month-name-to-number-of-days.py
@@ -1,44 +1,37 @@
-#
-# Nome del mese a numero di giorni La lunghezza di un mese varia da 28 a 31 giorni.
-# In questo esercizio creerete un programma che legge il nome di un mese dall'utente come una stringa.
-# Poi il tuo programma dovrebbe visualizzare il numero di giorni in quel mese. Visualizzate
-# "28 o 29 giorni" per febbraio, in modo da tenere conto degli anni bisestili.
-
 # refactoring nomi variabili e messaggi output
 
-#print(u'\u00E9')  e accentata
+mounth_28_29 = 'is 28 or 29 days'
+mounth_30 = 'is 30 days'
+mounth_31 = ' is 31 days'
 
-mese_28_29 = 'è di 28 o 29 giorni'
-mese_30 = 'è di 30 giorni'
-mese_31 = ' è di 31 giorni'
+#hard code by remove user input and run test
+mount_user_input = 'march'.upper()
+mount_user_input = input('Enter mounth ').upper()
 
-
-mese = 'marzo'.upper()
-
-if mese == 'GENNAIO':
-    print('Gennaio ' + mese_31)
-elif mese == 'FEBBRAIO':
-    print('Febbraio '+mese_28_29)
-elif mese == 'MARZO':
-    print('Marzo '+ mese_31)
-elif mese == 'APRILE':
-    print('Aprile '+mese_30)
-elif mese == 'MAGGIO':
-    print('Maggio '+ mese_31)
-elif mese == 'GIUGNO':
-    print('GIUGNO '+mese_30)
-elif mese == 'LUGLIO':
-    print('Luglio '+mese_31)
-elif mese == 'AGOSTO':
-    print('Agosto '+mese_31)
-elif mese == 'SETTEMBRE':
-    print('Settembre '+ mese_30)
-elif mese == 'OTTOBRE':
-    print('Ottobre '+mese_31)
-elif mese == 'NOVEMBRE':
-    print('Novembre '+ mese_30)
-elif mese == 'DICEMBRE':
-    print('Dicembre '+ mese_31)
+if mount_user_input == 'JANUARY':
+    print('January ' + mounth_31)
+elif mount_user_input == 'FEBRUARY':
+    print('February ' + mounth_28_29)
+elif mount_user_input == 'MARCH':
+    print('March ' + mounth_31)
+elif mount_user_input == 'APRIL':
+    print('April ' + mounth_30)
+elif mount_user_input == 'MAY':
+    print('May ' + mounth_31)
+elif mount_user_input == 'JUNE':
+    print('June ' + mounth_30)
+elif mount_user_input == 'JULY':
+    print('July ' + mounth_31)
+elif mount_user_input == 'AUGUST':
+    print('August ' + mounth_31)
+elif mount_user_input == 'SEPTEMBER':
+    print('September ' + mounth_30)
+elif mount_user_input == 'OCTOBER':
+    print('October ' + mounth_31)
+elif mount_user_input == 'NOVEMBER':
+    print('November ' + mounth_30)
+elif mount_user_input == 'DECEMBER':
+    print('December ' + mounth_31)
 else:
     print('mounth not valid!')
 
