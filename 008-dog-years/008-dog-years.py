54c42b7c8e28edafa867a485311b77dc23d47585
Francesco Ricci <francescosave.ricci@gmail.com>
2022-01-07 22:30:28 +0100

    refacoring exercise 008 by variables name and output message

--- 008-dog-years/008-dog-years.py
+++ 008-dog-years/008-dog-years.py
@@ -1,13 +1,3 @@
-#
-# ANNI CANE
-# Si dice comunemente che un anno umano equivale a 7 anni canini.
-# Tuttavia questa semplice conversione non riconosce che i cani raggiungono l'età adulta
-# in circa due anni. Di conseguenza, alcune persone credono che sia meglio contare ciascuno
-# dei primi due anni umani come 10,5 anni canini, e poi contare ogni anno umano aggiuntivo
-# come 4 anni canini. Scrivi un programma che implementi la conversione da anni umani ad anni
-# canini descritta nel paragrafo precedente. Assicuratevi che il vostro programma funzioni
-# correttamente per conversioni di meno di due anni umani e per conversioni di due o più anni umani.
-# Il tuo programma dovrebbe mostrare un messaggio di errore appropriato se l'utente inserisce un numero negativo.
 # refactoring nomi variabili e messaggi output
 
 #primi due anni  canini 10 anni e 6 mesi umani
@@ -15,14 +5,14 @@
 #terzo anno = due anni (21 umani)  + 4
 #quarto anno 21 + 4 + 4
 
-def controlla_input_anni():
+def verify_input_year():
     #controllare il tipo inserito
     #input_anni = input('Inserire anni canini')
     import re
     ripeti = True
 
     while True:
-        user_input = input("Inserire anni canini ")
+        user_input = input("Enter dog years ")
         num_format = re.compile(r'^\-?[1-9][0-9]*$')
         isInteger = re.match(num_format, user_input)
         if not isInteger:
@@ -32,25 +22,25 @@ def controlla_input_anni():
             break
 
 
-totale_anni_canini =  (controlla_input_anni())
+total_dog_years =  (verify_input_year())
 #totale_anni_canini = input('Inserire anni canini')
 
-print('ok number valid!')
+print('OK number valid!')
 
 #arrotonda per difetto
-totale_anni_umani = 0
-if (totale_anni_canini >= 1 and  totale_anni_canini <3):
-    totale_anni_umani = int(10.5 * totale_anni_canini)
-elif (totale_anni_canini >2):
-    eta_cucciolo = 2
-    eta_adulto= totale_anni_canini -2
-    totale_anni_umani = int(10.5 * eta_cucciolo)
-    totale_anni_umani += int(7 * eta_adulto)
+total_humans_years = 0
+if (total_dog_years >= 1 and  total_dog_years <3):
+    total_humans_years = int(10.5 * total_dog_years)
+elif (total_dog_years > 2):
+    age_puppy_dog = 2
+    age_dog= total_dog_years - 2
+    total_humans_years = int(10.5 * age_puppy_dog)
+    total_humans_years += int(7 * age_dog)
 
 #controllare input con una funzionericorsiva per controllare che il numero non sia ne negativo e ne eccessivo
 
 #totale_anni_umani = ?
-print('Il tuo cane ha: '+ str(totale_anni_umani) + ' anni umani' )
+print('\nYour dog is ' + str(total_humans_years) + ' human years old')
 
 
 
