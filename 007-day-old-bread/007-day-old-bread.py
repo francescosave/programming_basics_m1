f32463ec4b022cdee129094ccc4b9014e3f704c1
Francesco Ricci <francescosave.ricci@gmail.com>
2022-01-07 21:43:47 +0100

    refacoring exercise 007 by variables name and output message

--- 007-day-old-bread/007-day-old-bread.py
+++ 007-day-old-bread/007-day-old-bread.py
@@ -1,35 +1,36 @@
+# refactoring nomi variabili e messaggi output
 
 tabulate_label = 35
 format_two_decimal = "%.2f"
-prezzo_pagnotta_pz = 3.49
+bread_price_pz = 3.49
 currency_money = '$'
 
+#hard code
+#bread_numebr_buy = 13
+bread_numebr_buy = int(input('Bread Number pay: '))
+product_description = 'PANE VECCHIO'
 
-numero_pagnotte_acquistate = 13
-descrizione_articolo = 'PANE VECCHIO'
-#numero_pagnotte_acquistate = input('Numero pagnotte acquistate: ')
 
-
-#soloround A due decimali arrotonda difetto/eccesso sulla terza decimale ma non stmpa se 0 --> utilizzare spec.di formato
-totale_pagare = round(numero_pagnotte_acquistate * prezzo_pagnotta_pz,2)
-totale_sconto = round(totale_pagare * 0.60,2)
-totale_scontato = round(totale_pagare - totale_sconto,2)
+#solo round A due decimali arrotonda difetto/eccesso sulla terza decimale ma non stmpa se 0 --> utilizzare spec.di formato
+total_pay = round(bread_numebr_buy * bread_price_pz, 2)
+total_discount = round(total_pay * 0.60, 2)
+total_discounted = round(total_pay - total_discount, 2)
 
 #per le decimali 0 --> utilizzare spec.di formato
-numero_pagnotte_acquistate = str(numero_pagnotte_acquistate)
-totale_pagare = str(format_two_decimal % totale_pagare)
-totale_sconto = str(format_two_decimal % totale_sconto)
-totale_scontato = str(format_two_decimal % totale_scontato)
+bread_numebr_buy = str(bread_numebr_buy)
+total_pay = str(format_two_decimal % total_pay)
+total_discount = str(format_two_decimal % total_discount)
+total_discounted = str(format_two_decimal % total_discounted)
 
 # formattazione etichetta con fantastica funzione ljust
 print('-'*67)
-print('Descrizione'.ljust(tabulate_label) +'Q.ta'.ljust(10) +'Prezzo U.'.ljust(15) + 'Prezzo'.ljust(15))
-print(descrizione_articolo.ljust(29)+ str(numero_pagnotte_acquistate).rjust(10) + str(prezzo_pagnotta_pz).rjust(15) + (totale_pagare + '$').rjust(12))
+print('Description'.ljust(tabulate_label) +'Q.ty'.ljust(10) +'Price U.'.ljust(15) + 'Price'.ljust(15))
+print(product_description.ljust(29) + str(bread_numebr_buy).rjust(10) + str(bread_price_pz).rjust(15) + (total_pay + '$').rjust(12))
 print('-'*67)
 
-print(('Totale da pagare:').ljust(tabulate_label) + totale_pagare + currency_money)
-print('Totale sconto (60%):'.ljust(tabulate_label) + totale_sconto + currency_money)
-print('Totale scontato:'.ljust(tabulate_label) + totale_scontato + currency_money)
+print(('Total to pay is:').ljust(tabulate_label) + total_pay + currency_money)
+print('Total discount (60%):'.ljust(tabulate_label) + total_discount + currency_money)
+print('Total discounted:'.ljust(tabulate_label) + total_discounted + currency_money)
 print('-'*45)
 
 
